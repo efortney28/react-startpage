ec48816727a630bf9b6e0a696cdb58b639e879fb
Eric Fortney <efortney28@gmail.com>
2020-10-04 14:58:41 -0500

    possibly fixed api issue

--- src/components/weatherHelper.js
+++ src/components/weatherHelper.js
@@ -1,5 +1,5 @@
 export const GET_API_URL = (lat, long) => {
     const { REACT_APP_API_ID } = process.env
-    let apiURL = "http://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&appid=" + REACT_APP_API_ID
+    let apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&appid=" + REACT_APP_API_ID
     return(apiURL)
 }
